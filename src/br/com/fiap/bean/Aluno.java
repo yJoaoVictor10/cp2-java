e20f832f5c894e009472cf30b9ee97febfb82eda
yJoaoVictor10 <jvna07@gmail.com>
2025-04-18 13:58:12 -0300

    Checkpoint 02 Java Finalizado

---
+++ src/br/com/fiap/bean/Aluno.java
@@ -0,0 +1,66 @@
+package br.com.fiap.bean;
+import javax.swing.*;
+import java.time.LocalDate;
+
+public class Aluno {
+    private int registroMatricula;
+    private String nomeCompleto;
+    private int anoDeNascimento;
+
+    public Aluno() {
+
+    }
+
+    public Aluno(int registroMatricula, String nomeCompleto, int anoDeNascimento) {
+        this.registroMatricula = registroMatricula;
+        this.nomeCompleto = nomeCompleto;
+        this.anoDeNascimento = anoDeNascimento;
+    }
+
+    public int getRegistroMatricula() {
+        return registroMatricula;
+    }
+
+    public void setRegistroMatricula(int registroMatricula) {
+        try {
+            if (registroMatricula >= 80000 && registroMatricula <= 599999){
+                this.registroMatricula = registroMatricula;
+            }else{
+                throw new Exception("O registro da matrícula deve ser maior ou igual a 80000 e menor ou igual a 599999.");
+            }
+        }catch (Exception e){
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        }
+    }
+
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }
+
+    public void setNomeCompleto(String nomeCompleto) {
+        this.nomeCompleto = nomeCompleto;
+    }
+
+    public int getAnoDeNascimento() {
+        return anoDeNascimento;
+    }
+
+    public void setAnoDeNascimento(int anoDeNascimento) {
+        try {
+            LocalDate anoAtual = LocalDate.now();
+            if (anoDeNascimento >= 1945 && anoDeNascimento <= anoAtual.getYear()){
+                this.anoDeNascimento = anoDeNascimento;
+            }else{
+                throw new Exception("O ano de nascimento deve ser maior ou igual a 1945 e menor ou igual ao ano atual.");
+            }
+        }catch (Exception e){
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        }
+    }
+
+    public int calcularIdade(LocalDate dataAtual){
+        int anoAtual = dataAtual.getYear();
+        int idade = anoAtual - anoDeNascimento;
+        return idade;
+    }
+}
---
+++ src/br/com/fiap/main/MainAluno.java
@@ -0,0 +1,52 @@
+package br.com.fiap.main;
+
+import br.com.fiap.bean.Aluno;
+
+import javax.swing.*;
+import java.time.LocalDate;
+
+public class MainAluno {
+    public static void main(String[] args) {
+
+        Aluno aluno1 = new Aluno(565592, "André Emygdio", 2006);
+        Aluno aluno2 = new Aluno(561995, "Gabriel Lourenço Müller", 2006);
+
+        Aluno aluno3 = new Aluno();
+        Aluno aluno4 = new Aluno();
+
+        LocalDate dataAtual = LocalDate.now();
+
+        String auxiliar;
+        try {
+            auxiliar = JOptionPane.showInputDialog("Digite o RM do aluno 3: ");
+                aluno3.setRegistroMatricula(Integer.parseInt(auxiliar));
+            auxiliar = JOptionPane.showInputDialog("Digite o nome completo do aluno 3: ");
+                aluno3.setNomeCompleto(auxiliar);
+            auxiliar = JOptionPane.showInputDialog("Digite o ano de nascimento do aluno 3: ");
+                aluno3.setAnoDeNascimento(Integer.parseInt(auxiliar));
+
+            auxiliar = JOptionPane.showInputDialog("Digite o RM do aluno 4: ");
+                aluno4.setRegistroMatricula(Integer.parseInt(auxiliar));
+            auxiliar = JOptionPane.showInputDialog("Digite o nome completo do aluno 4: ");
+                aluno4.setNomeCompleto(auxiliar);
+            auxiliar = JOptionPane.showInputDialog("Digite o ano de nascimento do aluno 4: ");
+                aluno4.setAnoDeNascimento(Integer.parseInt(auxiliar));
+
+            String mensagem = String.format(
+                    "Aluno 1 -> RM: %d | Nome: %s | Idade: %d\n" +
+                            "Aluno 2 -> RM: %d | Nome: %s | Idade: %d\n" +
+                            "Aluno 3 -> RM: %d | Nome: %s | Idade: %d\n" +
+                            "Aluno 4 -> RM: %d | Nome: %s | Idade: %d",
+                    aluno1.getRegistroMatricula(), aluno1.getNomeCompleto(), aluno1.calcularIdade(dataAtual),
+                    aluno2.getRegistroMatricula(), aluno2.getNomeCompleto(), aluno2.calcularIdade(dataAtual),
+                    aluno3.getRegistroMatricula(), aluno3.getNomeCompleto(), aluno3.calcularIdade(dataAtual),
+                    aluno4.getRegistroMatricula(), aluno4.getNomeCompleto(), aluno4.calcularIdade(dataAtual)
+            );
+
+            JOptionPane.showMessageDialog(null, mensagem);
+
+        }catch (Exception e){
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        }
+    }
+}
